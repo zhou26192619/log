f94eb939a115849b0cfa543c18a0bdc5ef602413
LoaR <460891016@qq.com>
2016-08-09 17:50:05 +0800

    华为p9适配，结束launcher后状态栏屏蔽不消失，下载后不安装，辅助功能默认launcher选不上，打印日志修改

--- src/main/java/com/loar/log/LogManager.java
+++ src/main/java/com/loar/log/LogManager.java
@@ -1,5 +1,10 @@
 package com.loar.log;
 
+import java.io.PrintWriter;
+import java.io.StringWriter;
+import java.io.Writer;
+import java.util.Date;
+
 /**
  * Created by Justsy on 2016/7/11.
  */
@@ -66,5 +71,22 @@ public class LogManager {
 
     public static void warn(String tag, String msg) {
         LogManager.getRootLogger().warn(tag, msg);
+    }
+
+    public static void err(String tag, String head, Exception e) {
+        if (config.isDevelop() || Config.Level.ERROR.ordinal() >= config.setLevel().ordinal()) {
+            e.printStackTrace();
+        }
+        LogManager.getRootLogger().err(tag, head + " >> " + exceptionToString(e));
+    }
+
+    public static String exceptionToString(Exception e) {
+        if (e == null) {
+            return "";
+        }
+        Writer result = new StringWriter();
+        PrintWriter printWriter = new PrintWriter(result);
+        e.printStackTrace(printWriter);
+        return result.toString();
     }
 }
