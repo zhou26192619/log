479f76a73f54ecb8d1d87c6110e8408937800e42
LoaR <460891016@qq.com>
2016-09-13 11:00:56 +0800

    修改重要bug

--- src/main/java/com/loar/log/LogManager.java
+++ src/main/java/com/loar/log/LogManager.java
@@ -75,6 +75,7 @@ public class LogManager {
 
     public static void err(String tag, String head, Exception e) {
         if (tag == null || head == null || e == null) {
+
             return;
         }
         if (config.isDevelop() || Config.Level.ERROR.ordinal() >= config.setLevel().ordinal()) {
